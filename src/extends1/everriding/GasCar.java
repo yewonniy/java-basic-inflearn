ce3306980dc77491d1320cdcd9413d01c3a20772
yewonniy <ae6652@naver.com>
2024-04-22 20:34:26 +0900

    section9 상속

---
+++ src/extends1/everriding/GasCar.java
@@ -0,0 +1,7 @@
+package extends1.everriding;
+
+public class GasCar extends Car {
+    public void fillUp() {
+        System.out.println("기름을 주유합니다");
+    }
+}
---
+++ src/extends1/everriding/HydrogenCar.java
@@ -0,0 +1,8 @@
+package extends1.everriding;
+
+public class HydrogenCar extends Car {
+
+    public void fillHydrogen() {
+        System.out.println("수소를 충전합니다");
+    }
+}
